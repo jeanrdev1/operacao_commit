ba64b6f6e13421f80535e0da8e4c62c21c6e01d5
Betaonanet <betaonanet@gmail.com>
2023-05-16 22:45:51 -0300

    commit 22h45

--- valdemar/src/App.java
+++ valdemar/src/App.java
@@ -1,5 +1,17 @@
+package src;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Valdemar no Hello, World!");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite o texto: ");
+        String texto = sc.nextLine(); 
+    
+    sc.close();
+
+    TiraAcento txt = new TiraAcento(texto.toUpperCase());
+    System.out.println("Resultado: "+ txt.textofinal);
     }
 }
