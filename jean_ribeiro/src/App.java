78c324810021ced8d021ca02635b7c95ce47e93e
Samuel Fernandes <sargentosamuel@hotmail.com>
2023-05-17 10:22:23 -0300

    “aprendendo”

--- jean_ribeiro/src/App.java
+++ jean_ribeiro/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World PMGO 165 ANOS DE GLORIA!");
     }
 }
