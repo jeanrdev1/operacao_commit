b8b970beb1c2daf0b8498dad196f5dae90299e3e
Samuel Fernandes <sargentosamuel@hotmail.com>
2023-05-18 10:02:22 -0300

    editado vresao ii

--- jean_ribeiro/src/App.java
+++ jean_ribeiro/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World PMGO 165 ANOS DE GLORIA!");
+        System.out.println("Hello, World -- 165 A DE GLORIA!");
     }
 }
